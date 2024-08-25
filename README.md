2fc469b7bc8c01fde7a80bd49ccfc720f91fbcd0
JaviQuilumba <javier0611quilumba@gmail.com>
2024-08-25 10:10:20 -0500

    feat: :sparkles: Uploading the Frontend application

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Cinema Platform
+
+<p>
+This frontend simulates a cinema platform, it is designed to consume several microservices to perform its different actions. It accesses these microservices through defined URLs that point to the microservices defined in the backend. Each microservice has its internal logic to process the respective action. The availability of the functions will depend on the role that the user has at the moment of logging into the application.
+</p>
+
+
+#### How to install and run the project? :wrench:
+The project requires Node.js and npm installed on your system. To install and run this frontend you must perform the following steps:
+
+###### Clone the repository:
+
+- `git clone <URL_OF_REPOSITORY>`
+- `cd <URL_OF_REPOSITORY> `
+
+###### Install dependencies:
+
+- `npm install`
+
+###### Execute the server:
+- `npm start`
+
+
+> [!NOTE]
+> This fronted is done in REACT so the default port of execution is 3000.
+
+
+#### How to use the project :computer:
+<p>
+To use this application you need to install and run the respective microservices you want to test, and modify the URLs according to your needs, once the frontend and microservices are running you can use them.
+</p>
+
+#### Microservices repositories:
+
+- **Users**: <https://github.com/JaviQuilumba/MicroservicesUsers.git>
+- **Movies**: <https://github.com/JaviQuilumba/MicroservicesMovies.git>
+- **Snacks**: <https://github.com/JaviQuilumba/MicroservicesSnacks.git>
+- **Theaters**: <https://github.com/JaviQuilumba/MicroservicesTheaters.git>
+- **Info**: <https://github.com>
+- **Releases**: <https://github.com>
+
+#### Technologies used for these microservices
+- **Node.js** with the Express.js framework for the backend server.
+
+###  License 
+This project is licensed under the (AFL-3.0) License - see the [LICENSE](https://opensource.org/license/afl-3-0-php) file for details.
---
+++ src/components/registerUser.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import './styles/RegisterUser.css';
+import axios from 'axios';
+import config from '../config';
+
+
+const RegisterUser = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const role = 'client';
+  const navigate = useNavigate(); 
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newUser = { username, email, password, first_name, last_name, role };
+
+    try {
+      await axios.post(config.BASE_URL_REGISTER, newUser);
+      console.log('User registered:', newUser);
+      alert('User registered successfully!');
+      
+      setUsername('');
+      setEmail('');
+      setPassword('');
+      setFirstName('');
+      setLastName('');
+
+      navigate('/');
+    } catch (error) {
+      console.error('Error registering user:', error);
+      alert('Error registering user. Please try again.');
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register User</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <label>First Name:</label>
+        <input
+          type="text"
+          value={first_name}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+        <label>Last Name:</label>
+        <input
+          type="text"
+          value={last_name}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+        <label>Role:</label>
+        <input
+          type="text"
+          value={role}
+          readOnly
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterUser;
